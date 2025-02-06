a9b12393048119050d507c5746bf3643929c5e19
sigurdandris <sigurd.andris.sorengen@gmail.com>
2025-02-06 12:05:23 +0100

    made a main class and added maven build plugin

---
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/BoardGameApp.java
@@ -0,0 +1,7 @@
+package edu.ntnu.idi.idatt.boardgame;
+
+public class BoardGameApp {
+  public static void main(String[] args) {
+    System.out.println("Jeg trenger masse hjelp med å gå på do.");
+  }
+}
