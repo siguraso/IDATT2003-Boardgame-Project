849a936b3ae4a9ac5d26bf21122540dbf8c5e97f
Sigurd Andris Sorengen <sigurd.andris.sorengen@gmail.com>
2025-02-13 21:21:01 +0100

    made a very rough demo of what the game window will look like

--- src/main/java/edu/ntnu/idi/idatt/boardgame/view/BoardGameWindow.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/view/BoardGameWindow.java
@@ -1,8 +1,11 @@
 package edu.ntnu.idi.idatt.boardgame.view;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,7 +19,7 @@ import javafx.stage.Stage;
  */
 public class BoardGameWindow implements Window {
 
-  private Stage window = new Stage();
+  private final Stage window = new Stage();
 
   // methods for window initializing, opening and closing a window.
 
@@ -28,9 +31,15 @@ public class BoardGameWindow implements Window {
   @Override
   public void init() {
     // Initialize the window
-    Scene scene = new Scene(getBoardRegion(), 500, 500);
+    BorderPane root = new BorderPane();
+    root.setLeft(getBoardRegion());
+    root.setRight(getSidebar());
+
+    Scene scene = new Scene(root, 1400, 900);
+
     scene.getStylesheets().add("file:src/main/resources/Styles/Style.css");
 
+    window.setResizable(false);
     window.setScene(scene);
   }
 
@@ -43,25 +52,54 @@ public class BoardGameWindow implements Window {
 
   private StackPane getBoardRegion() {
     StackPane boardDisplay = new StackPane();
-    boardDisplay.setLayoutX(0);
-    boardDisplay.setLayoutY(0);
+    boardDisplay.setMinWidth(1000);
+    boardDisplay.setMinHeight(900);
     boardDisplay.getStyleClass().add("root");
 
     ImageView boardImage = new ImageView(
         new Image("file:src/main/resources/Images/placeholder.jpg"));
-    boardImage.setFitHeight(200);
-    boardImage.setFitWidth(200);
+    boardImage.setFitHeight(850);
+    boardImage.setFitWidth(850);
 
     boardDisplay.getChildren().add(boardImage);
 
     return boardDisplay;
   }
 
-  /*
   private VBox getSidebar() {
+    VBox sidebar = new VBox();
+    sidebar.setMinWidth(400);
+    sidebar.setPadding(new javafx.geometry.Insets(20, 10, 20, 10));
+    sidebar.setSpacing(20);
+    sidebar.setAlignment(javafx.geometry.Pos.CENTER);
 
+    // placeholder for dialog box
+    // TODO make a dialog box class that can be defined in the constructor
+    ImageView dialogPlaceholder = new ImageView(
+        new Image("file:src/main/resources/Images/placeholder.jpg"));
+    // IMPORTANT: dimentions of dialogbox: 380x200
+    dialogPlaceholder.setFitWidth(380);
+    dialogPlaceholder.setFitHeight(200);
+
+    // placeholder for die
+    // TODO get actual sprites for the die
+    ImageView diePlaceholder = new ImageView(
+        new Image("file:src/main/resources/Images/placeholder.jpg"));
+    diePlaceholder.setFitWidth(200);
+    diePlaceholder.setFitHeight(200);
+
+    // TODO make the button actually roll the die
+    Button rollDieButton = new Button("Roll die");
+    rollDieButton.getStyleClass().add("button");
+    rollDieButton.setOnAction(e -> {
+      rollDieButton.setDisable(true);
+      diePlaceholder.setImage(new Image("file:src/main/resources/Images/placeholder2.png"));
+    });
+
+    sidebar.getChildren().addAll(dialogPlaceholder, diePlaceholder, rollDieButton);
+
+    sidebar.getStyleClass().add("sidebar");
+    return sidebar;
   }
-
-   */
 
 }
